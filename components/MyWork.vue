f838e0c635ea842f901491f5c5242284335be7f4
K-Pinck <meefis6@gmail.com>
2023-09-17 16:36:50 -0500

    updated mywork grid section

--- components/MyWork.vue
+++ components/MyWork.vue
@@ -20,14 +20,19 @@
       <div v-for="work in mywork" :key="work.name">
         <!-- Card -->
         <div class="work-card w-4/5 border rounded-md mx-auto">
-          <div class="mywork-card_header w-full h-42 border overflow-hidden">
+          <div
+            class="mywork-card_header relative w-full h-[28vh] border-b overflow-hidden"
+          >
             <img
               :src="work.img"
               :alt="work.alt"
-              class="object-cover w-full h-full"
+              class="object-cover w-full h-full z-0 rounded-t-md"
             />
+            <div class="hover:bg-black/90 w-full h-full flex justify-center items-center absolute top-0 left-0 z-10 opacity-0 hover:opacity-100 transition-all duration-300 ease-in-out rounded-t-md">
+                <a :href="work.url" target="_blank" class="text-xl text-green-500 hover:bg-green-500 hover:text-white font-bold border border-green-500 rounded-full px-4 py-2 transition-all duration-300 ease-in-out">Visit Website</a>
+            </div>
           </div>
-          <div class="work-card_body flex flex-col gap-2 p-3">
+          <div class="work-card_body h-52 flex flex-col gap-2 p-3">
             <h3 class="text-2xl text-white font-bold drop-shadow-md">
               {{ work.name }}
             </h3>
@@ -47,28 +52,28 @@ const props = defineProps({
     required: true,
     default: () => [
       {
-        name: "Project 1",
+        name: "Secured Moving",
         description:
-          "This is a description of project 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget nisi vel ex aliquet facilisis. Fusce ut justo eget.",
-        url: "https://www.google.com",
-        img: "/code-icon.svg",
-        alt: "Project 1",
+          "Moving company based in DFW with nearly 20 years of experience behind them. They cover both local and long distance moves, and have a 5 star rating on Google.",
+        url: "https://www.securedmoving.com/",
+        img: "/SecuredMoving.jpg",
+        alt: "Secured Moving",
       },
       {
-        name: "Project 2",
+        name: "Blue Star Surveying",
         description:
-          "This is a description of project 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget nisi vel ex aliquet facilisis. Fusce ut justo eget.",
-        url: "https://www.google.com",
-        img: "/code-icon.svg",
-        alt: "Project 2",
+          "Local surveying company based out of Cleburne, TX. They offer a wide range of services, including boundary surveys, elevation certificates, and more.",
+        url: "https://bluestarsurveying.com/",
+        img: "/BlueStar.jpg",
+        alt: "Blue Star Surveying", 
       },
       {
-        name: "Project 3",
+        name: "USMRC",
         description:
-          "This is a description of project 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget nisi vel ex aliquet facilisis. Fusce ut justo eget.",
-        url: "https://www.google.com",
-        img: "/code-icon.svg",
-        alt: "Project 3",
+          "The USMRC at it’s core is a group of elite metal roofing companies who use their combined buying power to purchase the best products available at the lowest possible prices through bulk buying.",
+        url: "https://usmetalroofingconsortium.com/",
+        img: "/USMRC.jpg",
+        alt: "USMETALROOFINGCONSORTIUM.COM",
       },
     ],
   },
