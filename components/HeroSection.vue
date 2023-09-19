b00388aa09d4ad31725205507cf0b5362254aab8
K-Pinck <meefis6@gmail.com>
2023-09-19 13:23:15 -0500

    Added built-by text to the hero section

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,10 +1,19 @@
 <template>
   <section class="main-container sm:p-6">
     <div class="sm:w-1/2 border-b py-4">
-        <p class="text-2xl sm:text-3xl text-white font-sans font-bold">Hello, my name is</p>
-        <h1 class="text-4xl sm:text-6xl text-gray-300 font-sans font-bold">Michael Pinckard Jr.
-        </h1>
-        <p class="text-2xl sm:text-3xl text-white font-sans font-bold">But you can call me <span class="underline underline-offset-4 decoration-0">Keith</span></p>
+      <p class="text-2xl sm:text-3xl text-white font-sans font-bold">
+        Hello, my name is
+      </p>
+      <h1 class="text-4xl sm:text-6xl text-gray-300 font-sans font-bold">
+        Michael Pinckard Jr.
+      </h1>
+      <p class="text-2xl sm:text-3xl text-white font-sans font-bold">
+        But you can call me
+        <span class="underline underline-offset-4 decoration-0">Keith</span>
+      </p>
+      <p class="text-white italic font-thin pt-3">
+        This portfolio designed and built by me using Nuxt 3 and TailwindCSS
+      </p>
     </div>
   </section>
 </template>
