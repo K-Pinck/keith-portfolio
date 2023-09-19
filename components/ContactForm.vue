1b28856f19dd9d5dc5b1fed2e0288799f7841373
K-Pinck <meefis6@gmail.com>
2023-09-19 11:04:51 -0500

    remove heroicons

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -13,66 +13,129 @@
   ```
 -->
 <template>
-    <div class="isolate bg-white/50 rounded-md p-6">
-      <form action="#" method="POST" class="mx-auto  max-w-5xl">
-        <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-          <div>
-            <label for="first-name" class="block text-2xl font-semibold leading-6 text-white">First name</label>
-            <div class="mt-2.5">
-              <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6" />
-            </div>
-          </div>
-          <div>
-            <label for="last-name" class="block text-2xl font-semibold leading-6 text-white">Last name</label>
-            <div class="mt-2.5">
-              <input type="text" name="last-name" id="last-name" autocomplete="family-name" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6" />
-            </div>
-          </div>
-          <div class="sm:col-span-2">
-            <label for="company" class="block text-2xl font-semibold leading-6 text-white">Company</label>
-            <div class="mt-2.5">
-              <input type="text" name="company" id="company" autocomplete="organization" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6" />
-            </div>
-          </div>
-          <div class="sm:col-span-2">
-            <label for="email" class="block text-2xl font-semibold leading-6 text-white">Email</label>
-            <div class="mt-2.5">
-              <input type="email" name="email" id="email" autocomplete="email" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6" />
-            </div>
-          </div>
-          <div class="sm:col-span-2">
-            <label for="phone-number" class="block text-2xl font-semibold leading-6 text-white">Phone number</label>
-            <div class="relative mt-2.5">
-              <div class="absolute inset-y-0 left-0 flex items-center">
-                <label for="country" class="sr-only">Country</label>
-                <select id="country" name="country" class="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
-                  <option>US</option>
-                  <option>CA</option>
-                  <option>EU</option>
-                </select>
-                <ChevronDownIcon class="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400" aria-hidden="true" />
-              </div>
-              <input type="tel" name="phone-number" id="phone-number" autocomplete="tel" class="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6" />
-            </div>
-          </div>
-          <div class="sm:col-span-2">
-            <label for="message" class="block text-2xl font-semibold leading-6 text-white">Message</label>
-            <div class="mt-2.5">
-              <textarea name="message" id="message" rows="4" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6" />
-            </div>
+  <div class="isolate bg-white/50 rounded-md p-6">
+    <form action="#" method="POST" class="mx-auto max-w-5xl">
+      <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+        <div>
+          <label
+            for="first-name"
+            class="block text-2xl font-semibold leading-6 text-white"
+          >
+            First name
+          </label>
+          <div class="mt-2.5">
+            <input
+              type="text"
+              name="first-name"
+              id="first-name"
+              autocomplete="given-name"
+              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6"
+            />
           </div>
         </div>
-        <div class="mt-10">
-          <button type="submit" class="block w-full rounded-md bg-purple-700 px-3.5 py-2.5 text-center text-xl font-semibold text-white shadow-sm hover:bg-purple-600 ">Let's talk</button>
+        <div>
+          <label
+            for="last-name"
+            class="block text-2xl font-semibold leading-6 text-white"
+          >
+            Last name
+          </label>
+          <div class="mt-2.5">
+            <input
+              type="text"
+              name="last-name"
+              id="last-name"
+              autocomplete="family-name"
+              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6"
+            />
+          </div>
         </div>
-      </form>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue'
-  import { ChevronDownIcon } from '@heroicons/vue/20/solid/index.js'
-  import { Switch, SwitchGroup, SwitchLabel } from '@headlessui/vue'
-  
-  const agreed = ref(false)
-  </script>
+        <div class="sm:col-span-2">
+          <label
+            for="company"
+            class="block text-2xl font-semibold leading-6 text-white"
+          >
+            Company
+          </label>
+          <div class="mt-2.5">
+            <input
+              type="text"
+              name="company"
+              id="company"
+              autocomplete="organization"
+              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div class="sm:col-span-2">
+          <label
+            for="email"
+            class="block text-2xl font-semibold leading-6 text-white"
+          >
+            Email
+          </label>
+          <div class="mt-2.5">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              autocomplete="email"
+              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div class="sm:col-span-2">
+          <label
+            for="phone-number"
+            class="block text-2xl font-semibold leading-6 text-white"
+          >
+            Phone number
+          </label>
+          <div class="relative mt-2.5">
+            <div class="absolute inset-y-0 left-0 flex items-center">
+              <label for="country" class="sr-only">Country</label>
+            </div>
+            <input
+              type="tel"
+              name="phone-number"
+              id="phone-number"
+              autocomplete="tel"
+              class="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div class="sm:col-span-2">
+          <label
+            for="message"
+            class="block text-2xl font-semibold leading-6 text-white"
+          >
+            Message
+          </label>
+          <div class="mt-2.5">
+            <textarea
+              name="message"
+              id="message"
+              rows="4"
+              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-500 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="mt-10">
+        <button
+          type="submit"
+          class="block w-full rounded-md bg-purple-700 px-3.5 py-2.5 text-center text-xl font-semibold text-white shadow-sm hover:bg-purple-600"
+        >
+          Let's talk
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { Switch, SwitchGroup, SwitchLabel } from "@headlessui/vue";
+
+const agreed = ref(false);
+</script>
