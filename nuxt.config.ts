778766b8b88170e8e244786d0718f52d7ef404d0
K-Pinck <meefis6@gmail.com>
2023-09-19 10:49:04 -0500

    added hero icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "nuxt-headlessui"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image",
+    "nuxt-headlessui",
+    "nuxt-tailwind-heroicons",
+  ],
   alias: {
     assets: "/<rootDir>/assets",
   },
