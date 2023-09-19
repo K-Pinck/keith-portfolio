ee482e79666dd95a0c3769897b81df24a055e2f4
K-Pinck <meefis6@gmail.com>
2023-09-19 10:52:31 -0500

    Removed tailwind heroicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxt/image",
-    "nuxt-headlessui",
-    "nuxt-tailwind-heroicons",
-  ],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "nuxt-headlessui"],
   alias: {
     assets: "/<rootDir>/assets",
   },
