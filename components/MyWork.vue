98086e5440b07eeda7c6c927d5fd85a8b0d38653
K-Pinck <meefis6@gmail.com>
2024-07-25 12:54:30 -0500

    corrected name on hivolt

--- components/MyWork.vue
+++ components/MyWork.vue
@@ -70,7 +70,7 @@ const props = defineProps({
         description:
           "An east coast electrical company specializing in industrial, commercial and construction electrical installation, repair and maintenance.",
         url: "https://hivolt.maverickbuild.com",
-        img: "/public/Hi-Voltage.jpg",
+        img: "/HiVoltage.jpg",
         alt: "Hi Voltage Electric",
       },
       {
